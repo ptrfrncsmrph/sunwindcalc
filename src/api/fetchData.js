4994511099b87437a973caa4c1732e5d873a36cf
Peter Murphy <peterfrancismurphy@gmail.com>
2018-06-18 08:58:55 -0400

    After removing Folktale

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,29 +1,7 @@
-import { Left, Right, either, bimap, show, tagBy, unchecked } from "sanctuary"
+import { unchecked } from "sanctuary"
 import { encaseP, tryP, of as future, mapRej } from "fluture"
-import { chain, compose, map } from "ramda"
-import { create, env } from "sanctuary-def"
-import { env as flutureEnv } from "fluture-sanctuary-types"
+import { chain, compose } from "ramda"
 
-import Result from "folktale/result"
-import fetch from "node-fetch"
-
-const def = create({ checkTypes: true, env: env.concat(flutureEnv) })
-const { Ok, Error } = Result
-
-// const fetch = url =>
-//   Promise.resolve({
-//     status: 200,
-//     statusText: "OK",
-//     ok: true,
-//     value: { json: () => Promise.resolve("Whats this") }
-//   })
-
-const trace = x => {
-  console.log(x)
-  return x
-}
-
-const id = x => x
 const isOk = ({ ok }) => !!ok
 
 // parseJson :: Promise e r -> Future e r
@@ -33,25 +11,8 @@ const parseJson = ({ value: response }) =>
     return response.json().then(data => ({ data, status, statusText }))
   })
 
-// console.log(either(show)(show)(Left("poop")))
-
-// export default compose(
-//   mapRej(({ message, name }) => ({ message, name })),
-//   // x => x.fork(console.log, console.log),
-//   chain(
-//     parseJson
-//   ),
-//   chain(
-//     compose(
-//       future,
-//       fromPredicate(isOk)
-//     )
-//   ),
-//   // traceMap(id)(id),
-//   encaseP(fetch)
-// )
-
-compose(
+// fetchData :: URL -> Future e r
+export default compose(
   mapRej(({ message, name }) => ({ message, name })),
   chain(parseJson),
   chain(
@@ -61,30 +22,4 @@ compose(
     )
   ),
   encaseP(fetch)
-)("https://swapi.co/api/planets/331/?format=json").fork(
-  x => console.log("Something went wrong"),
-  console.log
 )
-
-// compose(
-//   mapRej(({ message, name }) => ({ message, name })),
-//   chain(x =>
-//     x.matchWith({
-//       Error: compose(
-//         map(Error),
-//         parseJson
-//       ),
-//       Ok: compose(
-//         map(Ok),
-//         parseJson
-//       )
-//     })
-//   ),
-//   chain(
-//     compose(
-//       future,
-//       fromPredicateB(isOk)
-//     )
-//   ),
-//   encaseP(fetch)
-// )("https://swapi.co/api/planets/33889/?format=json").fork(console.log, console.log)
